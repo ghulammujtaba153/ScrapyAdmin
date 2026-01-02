c72998d3e9d2b11556b0e833007b97656b21f795
ghulammujtaba153 <ghulammujtaba.dro@gmail.com>
2026-01-02 11:40:43 +0500

    url flow updated

--- src/config/url.js
+++ src/config/url.js
@@ -1,4 +1,4 @@
-// export const BASE_URL = "http://localhost:5000/api";
+export const BASE_URL = `${import.meta.env.VITE_BASE_URL}/api`;
 
 
-export const BASE_URL = "https://scrappyeasyserver.onrender.com/api";
+// export const BASE_URL = "https://scrappyeasyserver.onrender.com/api";
